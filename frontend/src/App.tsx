eeaacfbc3a843b6e9d04bd467f91f2e2ba21cbee
ricardoagnello <ricardo.agnello@outlook.com>
2022-09-10 09:18:37 -0300

    Other Components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,20 @@
-import NotificationButton from "./components/NotificationButton"
- 
+import Header from "./components/Header";
+import NotificationButton from "./components/NotificationButton";
+import SalesCard from "./components/SalesCard";
 
 function App() {
-  return(
-   <NotificationButton/>
-    
-  )
+  return (
+    <>
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard/>
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
